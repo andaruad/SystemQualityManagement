a1b2ac1b93e53d64efaf5adf5a376824aa0f17f7
Andaru Adiwignya <andaruad@gmail.com>
2015-12-09 14:18:27 +0800

    initial

---
+++ SQM/src/g53sqm/jibble/JibbleTest.java
@@ -0,0 +1,14 @@
+package g53sqm.jibble;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JibbleTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
